dcbd6132283a75acad3ff32b8cd8e9e543ebc865
tmhassan <78134452+tmhassan@users.noreply.github.com>
2023-08-30 20:21:00 -0500

    first commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tmhassan/Documents/projects/HelloWorld
+# Build directory: /Users/tmhassan/Documents/projects/HelloWorld/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Taariq Hassan on 8/30/23.
+//
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "Hello World" << endl;
+
+    return 0;
+}
